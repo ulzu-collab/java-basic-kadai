f39181cc2350001ee08ad030279cf893f6b5560b
ulzu-collab <ulzu@outlook.jp>
2025-12-04 18:16:19 +0900

    辞書への登録と検索と出力のコードをDictionaryに移動し、実行ファイルではメソッドの呼び出しのみに変更

--- kadai_021/DictionaryExec_Chapter21.java
+++ kadai_021/DictionaryExec_Chapter21.java
@@ -1,33 +1,14 @@
 package kadai_021;
 
-public class DictionaryExec_Chapter21 extends Dictionary_Chapter21 {
+public class DictionaryExec_Chapter21 {
 
 	public static void main(String[] args) {
 		
-	dialy("apple", "りんご");
-	dialy("peach", "桃");
-	dialy("banana", "バナナ");
-	dialy("lemon", "レモン");
-	dialy("pear", "梨");
-	dialy("kiwi", "キウィ");
-	dialy("strawberry", "いちご");
-	dialy("grape", "ぶどう");
-	dialy("muscat", "マスカット");
-	dialy("cherry", "さくらんぼ");
+		Dictionary_Chapter21  List = new Dictionary_Chapter21();
 		
-	//System.out.println(dictionary.get("apple"));
-	
-	String[] searchWords = { "apple", "banana", "grepe", "orenge" };
-	
-	for (String word : searchWords ) {
-		if(dictionary.containsKey(word)) {
-			System.out.println(word + "の意味は" + dictionary.get(word));
-		} else {
-			System.out.println(word + "は辞書に存在しません");
-		}
+		List.dialy();
 		
-	}
-	
+		List.search();
 	}
 
 }
--- kadai_021/Dictionary_Chapter21.java
+++ kadai_021/Dictionary_Chapter21.java
@@ -7,10 +7,31 @@ public class Dictionary_Chapter21 {
 	
 	static HashMap<String, String> dictionary = new HashMap<String, String>();
 	
-	public static String dialy(String index, String answer) {
+	public void dialy() {
 		
-		dictionary.put(index, answer);
-		return answer;
+		dictionary.put("apple", "りんご");
+		dictionary.put("peach", "桃");
+		dictionary.put("banana", "バナナ");
+		dictionary.put("lemon", "レモン");
+		dictionary.put("pear", "梨");
+		dictionary.put("kiwi", "キウィ");
+		dictionary.put("strawberry", "いちご");
+		dictionary.put("grape", "ぶどう");
+		dictionary.put("muscat", "マスカット");
+		dictionary.put("cherry", "さくらんぼ");
+		
 		}
+	
+	public void search() {
+		String[] searchWords = { "apple", "banana", "grape", "orange" };
+		
+		for (String word : searchWords ) {
+			if(dictionary.containsKey(word)) {
+				System.out.println(word + "の意味は" + dictionary.get(word));
+			} else {
+				System.out.println(word + "は辞書に存在しません");
+			}
+		}
+	}
 	
 }
